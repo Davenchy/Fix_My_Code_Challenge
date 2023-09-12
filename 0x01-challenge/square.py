df8d7e450f4b957b4bd5b99f285b4a971e10c07a
Fadi Asaad <firon1222@gmail.com>
2023-09-12 15:09:28 +0300

    update square (task 1): add str docs

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+"""My square class definition"""
+
 
 class square():
-
+    """My square class"""
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """Create a new square class instance
+        Args:
+            width (int): the square width
+            height (int): the square height"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -14,6 +20,7 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ calculates and returns the permiter of my square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
@@ -21,6 +28,7 @@ class square():
 
 
 if __name__ == "__main__":
+    """Prints square area and permiter"""
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
