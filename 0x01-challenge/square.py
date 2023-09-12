4247ce1fba28b0f14f452d0efb928cee5ad25bd6
Fadi Asaad <firon1222@gmail.com>
2023-09-12 15:12:38 +0300

    fuck this task

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """My square class definition"""
 
 
-class square():
+class Square():
     """My square class"""
     width = 0
     height = 0
@@ -24,12 +24,13 @@ class square():
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """My class str representation"""
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
     """Prints square area and permiter"""
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.permiter_of_my_square())
